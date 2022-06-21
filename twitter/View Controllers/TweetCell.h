de9f028d07ef28fca98cd63ba4d83d503b34f71b
Andrea Gonzalez <andreagonz@fb.com>
2022-06-21 09:26:21 -0700

    Add Tweet cell and outlets. Add GET request

---
+++ twitter/View Controllers/TweetCell.h
@@ -0,0 +1,23 @@
+//
+//  TweetCell.h
+//  twitter
+//
+//  Created by Andrea Gonzalez on 6/21/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TweetCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *tweetDateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *userNameLabel;
+
+@property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
+@property (weak, nonatomic) IBOutlet UILabel *fullNameLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
