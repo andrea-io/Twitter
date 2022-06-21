64c81c3862b8a433bd8bb22307bc4edc483f3692
Andrea Gonzalez <andreagonz@fb.com>
2022-06-21 15:57:45 -0700

    Beginning of Compose Tweet wiring

--- twitter/View Controllers/TimelineViewController.h
+++ twitter/View Controllers/TimelineViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "ComposeViewController.h"
 
 @interface TimelineViewController : UIViewController
 
--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -16,7 +16,7 @@
 #import "Tweet.h"
 #import "User.h"
 
-@interface TimelineViewController () <UITableViewDelegate, UITableViewDataSource>
+@interface TimelineViewController () <ComposeViewControllerDelegate, UITableViewDataSource, UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 - (IBAction)didTapLogout:(id)sender;
 
@@ -59,15 +59,11 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    UINavigationController *navigationController = [segue destinationViewController];
+    ComposeViewController *composeController = (ComposeViewController*)navigationController.topViewController;
+    composeController.delegate = self;
 }
-*/
 
 - (IBAction)didTapLogout:(id)sender {
     // Access the instance of the application delegate
