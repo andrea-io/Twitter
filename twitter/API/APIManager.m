53ce760301d5bf2f37da7547e7d650b197a1faa6
Andrea Gonzalez <andreagonz@fb.com>
2022-06-22 13:15:44 -0700

    didTapRetweet function, needs UI refresh function

--- twitter/API/APIManager.m
+++ twitter/API/APIManager.m
@@ -96,4 +96,17 @@ static NSString * const baseURLString = @"https://api.twitter.com";
     }];
 }
 
+// Takes in the tweet to be retweeted and then uses its id property to create the parameters dictionary.
+- (void)retweet:(Tweet *)tweet completion:(void (^)(Tweet *, NSError *))completion {
+
+    NSString *urlString = @"/1.1/statuses/retweet/:id.json";
+    NSDictionary *parameters = @{@"id": tweet.idStr};
+    [self POST:urlString parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, NSDictionary *  _Nullable tweetDictionary) {
+        Tweet *tweet = [[Tweet alloc]initWithDictionary:tweetDictionary];
+        completion(tweet, nil);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        completion(nil, error);
+    }];
+}
+
 @end
--- twitter/TweetCell.m
+++ twitter/TweetCell.m
@@ -9,13 +9,38 @@
 #import "TweetCell.h"
 #import "APIManager.h"
 
+#import "AppDelegate.h"
+#import "UIImageView+AFNetworking.h"
+#import "ComposeViewController.h"
+#import "TimelineViewController.h"
+#import "Tweet.h"
+#import "User.h"
+
 @implementation TweetCell
+
+- (IBAction)didTapRetweet:(id)sender {
+    // TODO: Update the local tweet model
+    self.tweet.retweeted = YES;
+    self.tweet.retweetCount += 1;
+    
+    // TODO: Update cell UI
+    // Need to create a refreshData() method in the cell that updates all views, i.e. sets the labels to their respective text, etc.
+    
+    // TODO: Send a POST request to the POST favorites/create endpoint
+    [[APIManager shared] retweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+         if(error){
+              NSLog(@"Error retweeting tweet: %@", error.localizedDescription);
+         }
+         else{
+             NSLog(@"Successfully retweeted the following Tweet: %@", tweet.text);
+         }
+     }];
+}
 
 - (IBAction)didTapFavorite:(id)sender {
     // TODO: Update the local tweet model
     self.tweet.favorited = YES;
     self.tweet.favoriteCount += 1;
-    
     
     // TODO: Update cell UI
     // Need to create a refreshData() method in the cell that updates all views, i.e. sets the labels to their respective text, etc.
