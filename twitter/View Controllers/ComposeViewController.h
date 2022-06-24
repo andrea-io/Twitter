a31a0c08ae62d1e52c9a6433f05af27a704f9513
Andrea Gonzalez <andreagonz@fb.com>
2022-06-24 10:05:10 -0700

    Character limit stretch feature added, autolayout fixed

--- twitter/View Controllers/ComposeViewController.h
+++ twitter/View Controllers/ComposeViewController.h
@@ -14,11 +14,13 @@ NS_ASSUME_NONNULL_BEGIN
 @protocol ComposeViewControllerDelegate
 
 - (void)didTweet:(Tweet *)tweet;
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text;
 
 @end
 
 @interface ComposeViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *composeTextMaxCharacter;
 @property (weak, nonatomic) IBOutlet UITextView *composeTextView;
 @property (nonatomic, weak) id<ComposeViewControllerDelegate> delegate;
 
--- twitter/View Controllers/ComposeViewController.m
+++ twitter/View Controllers/ComposeViewController.m
@@ -36,6 +36,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.composeTextView.delegate = self;
+}
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
+    // Set the max character limit
+    int characterLimit = 140;
+
+    // Construct what the new text would be if we allowed the user's latest edit
+    NSString *newText = [self.composeTextView.text stringByReplacingCharactersInRange:range withString:text];
+    int charactersLeft = 140 - newText.length;
+    
+    // TODO: Update character count label
+    self.composeTextMaxCharacter.text = [NSString stringWithFormat:@"%d", charactersLeft];
+    
+    // Should the new text should be allowed? True/False
+    return newText.length < characterLimit;
 }
 
 /*
